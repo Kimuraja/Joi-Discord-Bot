b03787b60bdd447ed9bc55f0160bddb0a5f44d92
Jakub Załęcki <jakub.zalecki@gmail.com>
2023-04-24 16:10:40 +0200

    Hotfix

--- bot.py
+++ bot.py
@@ -3,20 +3,40 @@ import responses
 
 intents = discord.Intents.default()
 intents.typing = True
-async def send_message(message, user_msg, is_private):
+
+
+async def send_message(message, user_message, is_private):
     try:
-        response = responses.handle_response(user_msg)
+        response = responses.handle_response(user_message)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
 
 
 def run_discord_bot():
-    TOKEN = "MTA5OTY4MTMzMDI0NzIzNzY2Mg.G-Qv-y.smOXqBGx86wXtGInaRFR2Fsn0EEuZXI0FZjtIM"
+    TOKEN = "Your Token"
     client = discord.Client(intents=intents)
+
 
     @client.event
     async def on_ready():
         print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        if user_message == str:
+            user_message = user_message[1:]
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
 
     client.run(TOKEN)
