cc89d506f9bff72c09f6fef57856d6a95a7068f6
Zala145 <jakub.zalecki@gmail.com>
2023-06-03 19:35:42 +0200

    End of the day

--- bot.py
+++ bot.py
@@ -1,23 +1,39 @@
 import discord
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import dotenv_values
 from discord.ext import commands
-import os
-import asyncio
-import youtube_dl
 
-TOKEN = "Your Token"
-intents = discord.Intents().all()
+config = dotenv_values(".env")
+
 # API_KEY = "Your Token"
+TOKEN = ""
+intents = discord.Intents().all()
 client = commands.Bot(command_prefix=">", intents=intents, help_command=None)
-voice_clients = {}
 
-yt_dl_opts = {'format': 'bestaudio/best'}
-ytdl = youtube_dl.YoutubeDL(yt_dl_opts)
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=config['CLIENT_ID'], client_secret=config['CLIENT_SECRET']))
 
-ffmpeg_options = {'options': "-vn"}
+
+# TODO #1 Make bot play music from spotify:
+# Read the documentation
+# Make connection
+# Find music
+# Play music
+# TODO #2 Make bot warn/kick/ban/ping user:
+# Ping user
+# Then Warn user with a reason
+# Kick
+# Ban
+# TODO #3 Check if there is any possibility to play music from youtube one a bot:
+# Check other library than youtube_dl
+# TODO #5 Find out if there is any possibility to change CHATGPT into other AI:
+# Check other library than ChatGPT or find sugar mommy that will pay for it
+
+"""Below are the functional commands used by users to interact with the bot."""
 
 
 def run_discord_bot():
-    @client.group()
+    @client.command()
     async def help(ctx):
         em = discord.Embed(title=f"Hey, **{ctx.author}**, I am Joi", description="```A Discord Music Bot With Many "
                                                                                  "Awesome Features, Buttons, Menus, "
@@ -35,49 +51,43 @@ def run_discord_bot():
     @client.command()
     async def kick(ctx):
         em = discord.Embed(title="**Kick**",
-                           description="Get rid of the replicant from this server using this command."
-                           , color=ctx.author.color)
+                           description="Get rid of the replicant from this server using this command.",
+                           color=ctx.author.color)
         em.add_field(name="**SYNTAX**", value="```>kick <member> [reason]```")
         await ctx.reply(embed=em)
 
     @client.command()
     async def ban(ctx):
         em = discord.Embed(title="**Ban**",
-                           description="Make sure the replicant doesn't come back using this command."
-                           , color=ctx.author.color)
+                           description="Make sure the replicant doesn't come back using this command.",
+                           color=ctx.author.color)
         em.add_field(name="**SYNTAX**", value="```>ban <member> [reason]```")
         await ctx.reply(embed=em)
 
     @client.command()
     async def warn(ctx):
-        em = discord.Embed(title="**Warn**", description="SOON"
-                           , color=ctx.author.color)
+        em = discord.Embed(title="**Warn**", description="SOON",
+                           color=ctx.author.color)
         em.add_field(name="**SYNTAX**", value="```>help warn <member> [reason]```")
         await ctx.reply(embed=em)
 
     @client.command()
     async def remind(ctx):
-        em = discord.Embed(title="**Reminder**", description="SOON"
-                           , color=ctx.author.color)
+        em = discord.Embed(title="**Reminder**", description="SOON",
+                           color=ctx.author.color)
         em.add_field(name="**SYNTAX**", value="```>remind <member> [What did you want to be reminded of]```")
         await ctx.reply(embed=em)
 
     @client.command()
-    async def play(ctx, msg):
-        try:
-            url = msg
-            voice_client = await ctx.author.voice.channel.connect()
-            voice_clients[voice_client.guild.id] = voice_client
-
-            loop = asyncio.get_event_loop()
-            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False, extra_info={'verbose': True}))
-
-            song = data['url']
-            player = discord.FFmpegPCMAudioSource()
-
-            voice_client.play(player)
-        except Exception as err:
-            print(err)
+    async def play(ctx, prompt):
+        voice_channel = ctx.author.voice.channel
+        voice_client = await voice_channel.connect()
+        track_url = prompt
+        track_info = spotify.track(track_url)
+        if track_info:
+            track_preview_url = track_info['album']['external_urls']['spotify']
+            if track_preview_url:
+                voice_client.play(discord.FFmpegPCMAudio(track_preview_url))
 
     @client.command(pass_context=True)
     async def leave(ctx):
@@ -91,27 +101,27 @@ def run_discord_bot():
             em.add_field(name="JOI Music", value="```An Error occurred, I am not in a vc```")
             await ctx.reply(embed=em)
 
-    # --- After paying for the ChatGPT subscription, you will have the ability
-    # to use it in your bot using the code below.--- #
-
-    # @client.command()
-    # async def gpt(ctx: commands.Context, *, prompt: str):
-    #     print(prompt)
-    #     async with aiohttp.ClientSession() as session:
-    #         payload = {
-    #             "model": "text-davinci-002",
-    #             "prompt": prompt,
-    #             "temperature": 0.5,
-    #             "max_tokens": 50,
-    #             "presence_penalty": 0,
-    #             "frequency_penalty": 0,
-    #             "best_of": 1,
-    #         }
-    #         headers = {"Authorization": f"Bearer {API_KEY}"}
-    #         async with session.post("https://api.openai.com/v1/completions", json=payload, headers=headers) as resp:
-    #             print(resp)
-    #             response = await resp.json()
-    #             em = discord.Embed(title="**GPT**", description=f"```{response}```"
-    #                                , color=ctx.author.color)
-    #             await ctx.reply(embed=em)
     client.run(TOKEN)
+
+
+"""After paying for the ChatGPT subscription, you will have the ability to use it in your bot using the code below"""
+# @client.command()
+# async def gpt(ctx: commands.Context, *, prompt: str):
+#     print(prompt)
+#     async with aiohttp.ClientSession() as session:
+#         payload = {
+#             "model": "text-davinci-003",
+#             "prompt": prompt,
+#             "temperature": 0.5,
+#             "max_tokens": 50,
+#             "presence_penalty": 0,
+#             "frequency_penalty": 0,
+#             "best_of": 1,
+#         }
+#         headers = {"Authorization": f"Bearer {API_KEY}"}
+#         async with session.post("https://api.openai.com/v1/completions", json=payload, headers=headers) as resp:
+#             print(resp)
+#             response = await resp.json()
+#             em = discord.Embed(title="**GPT**", description=f"```{response}```"
+#                                , color=ctx.author.color)
+#             await ctx.reply(embed=em)
--- music.py
+++ music.py
@@ -1,31 +1,47 @@
-import discord
-import os
-import asyncio
-import youtube_dl
-from bot import client, intents
-
-voice_client = {}
-
-yt_dl_opts = {'format': 'bestaudio/best'}
-ytdl = youtube_dl.YoutubeDL(yt_dl_opts)
-
-ffmpeg_options = {'options': "-vn"}
-
-@client.event
-async def on_msg(msg):
-    if msg.content.startwith(">play"):
-        try:
-            url = msg.content.split()[1]
-
-            voice_client = await msg.author.voice.channel.connect()
-            voice_client[voice_client.id] = voice_client
-
-            loop = asyncio.get_event_loop()
-            data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=False))
-
-            song = data['url']
-            player = discord.FFmpegPCMAudio(song, **ffmpeg_options)
-
-            voice_client.play(player)
-        except Exception as err:
-            print(err)
+# import discord
+# import wavelink
+# from discord.ext import commands
+#
+#
+# class Bot(commands.Bot):
+#
+#     def __init__(self) -> None:
+#         intents = discord.Intents.default()
+#         intents.message_content = True
+#
+#         super().__init__(intents=intents, command_prefix='?')
+#
+#     async def on_ready(self) -> None:
+#         print(f'Logged in {self.user} | {self.user.id}')
+#
+#     async def setup_hook(self) -> None:
+#         # Wavelink 2.0 has made connecting Nodes easier... Simply create each Node
+#         # and pass it to NodePool.connect with the client/bot.
+#         node: wavelink.Node = wavelink.Node(uri='http://localhost:2333', password='youshallnotpass')
+#         await wavelink.NodePool.connect(client=self, nodes=[node])
+#
+#
+# bot = Bot()
+#
+#
+# @bot.command()
+# async def play(ctx: commands.Context, *, search: str) -> None:
+#     """Simple play command."""
+#
+#     if not ctx.voice_client:
+#         vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
+#     else:
+#         vc: wavelink.Player = ctx.voice_client
+#
+#     track = await wavelink.YouTubeTrack.search(search, return_first=True)
+#     await vc.play(track)
+#
+#
+# @bot.command()
+# async def disconnect(ctx: commands.Context) -> None:
+#     """Simple disconnect command.
+#
+#     This command assumes there is a currently connected Player.
+#     """
+#     vc: wavelink.Player = ctx.voice_client
+#     await vc.disconnect()
