190934100555550e42d7ad1543a84fb27c2a660f
Zala145 <jakub.zalecki@gmail.com>
2023-06-28 19:13:34 +0200

    End of the day

--- bot.py
+++ bot.py
@@ -1,13 +1,12 @@
 import asyncio
 import discord
 from datetime import datetime
-import youtube_dl
+from youtube_dl import YoutubeDL
 from dotenv import dotenv_values
 from discord.ext import commands
-import traceback
-# from discord import Member
-# from discord.ext.commands import has_permissions, MissingPermissions
 
+music_queue = []
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
 FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 intents = discord.Intents().all()
 config = dotenv_values("data.env")
@@ -16,8 +15,6 @@ client = commands.Bot(command_prefix=">", intents=intents, help_command=None)
 
 # TODO #1 Make bot play music from spotify/youtube:
 # Read the documentation
-# Make connection
-# Find music
 # Play music
 # TODO #3 Check if there is any possibility to play music from youtube one a bot:
 # Check other library than youtube_dl
@@ -27,9 +24,9 @@ client = commands.Bot(command_prefix=">", intents=intents, help_command=None)
 
 
 def run_discord_bot():
-    client = commands.Bot(command_prefix=">", intents=intents, help_command=None)
+    bot = commands.Bot(command_prefix=">", intents=intents, help_command=None)
 
-    @client.group()
+    @bot.group()
     async def help(ctx):
         em = discord.Embed(title=f"Hey, **{ctx.message.author.name}**, I am Joi",
                            description="```A Discord Music Bot With Many "
@@ -44,25 +41,7 @@ def run_discord_bot():
         em.add_field(name="**Fun**", value="`Music YT, Spotify` SOON\n")
         await ctx.reply(embed=em)
 
-    @client.event
-    async def on_command_error(ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            await ctx.send('Please pass in all requirements :rolling_eyes:.')
-        elif isinstance(error, commands.CommandInvokeError):
-            await ctx.send("Error occurred")
-        elif isinstance(error, discord.errors.Forbidden):
-            await ctx.send("You dont have all the requirements")
-        elif isinstance(error, commands.MemberNotFound):
-            await ctx.send("Member not found")
-        elif isinstance(error, commands.MissingPermissions):
-            if ctx.command.name in ['kick', 'ban', 'warn']:
-                await ctx.send("You don't have the required permissions to run this command.")
-            else:
-                await ctx.send("You dont have all the requirements :angry:")
-        else:
-            print(''.join(traceback.format_exception(type(error), error, error.__traceback__)))
-
-    @client.command(name="kick", pass_context=True)
+    @bot.command(name="kick", pass_context=True)
     @commands.has_permissions(manage_roles=True, kick_members=True)
     async def kick(ctx, user: discord.Member, error):
         try:
@@ -84,8 +63,7 @@ def run_discord_bot():
             if isinstance(error, commands.errors.MissingPermissions):
                 await ctx.send('MissingPermissions...')
 
-
-    @client.command()
+    @bot.command()
     @commands.has_permissions(ban_members=True)
     async def ban(ctx, user: discord.Member, *, reason=None):
         try:
@@ -97,7 +75,8 @@ def run_discord_bot():
                 command = ctx.message.content.lower()
                 msg = command.split(">ban")[1].strip()
                 r = msg.split(" for ")
-                em = discord.Embed(title="**Ban**", description=f"Replicant {user} has been purged", color=ctx.author.color)
+                em = discord.Embed(title="**Ban**", description=f"Replicant {user} has been purged",
+                                   color=ctx.author.color)
                 await user.send(f"You've been banned for {r[1]}")
                 await user.ban(reason=reason)
                 await ctx.reply(embed=em)
@@ -105,7 +84,7 @@ def run_discord_bot():
             em = discord.Embed(title="**Error**", description=f"{err}")
             await ctx.reply(embed=em)
 
-    @client.command()
+    @bot.command()
     @commands.has_permissions(mute_members=True)
     async def warn(ctx, *, user: discord.Member):
         try:
@@ -126,7 +105,7 @@ def run_discord_bot():
             em = discord.Embed(title="**Error**", description=f"{err}")
             await ctx.reply(embed=em)
 
-    @client.command()
+    @bot.command()
     async def remind(ctx):
         if ctx.message.content == ">remind help":
             em = discord.Embed(color=ctx.author.color)
@@ -149,26 +128,27 @@ def run_discord_bot():
                          value=f"{ctx.message.author.mention} You wanted me to remind you about: {input_time[0]}")
             await ctx.send(embed=em)
 
-    @client.command() #TODO Work this out and test on a vc
-    #Additional command access to use this command
+    @bot.command()
+    # Additional command access to use this command
     async def play(ctx):
-
-        if ctx.voice_client:
-            em = discord.Embed()
-            em.add_field(name="JOI Music", value="```I'm on vc```")
-            await ctx.reply(embed=em)
+        if not ctx.author.voice.channel:
+            await ctx.send("You're not on a voice channel")
         else:
-            url = ctx.message.content
-            ydl_opts = {'format': 'bestaudio'}
-            vid_link = url.split(">play ")[1]
-            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            URL = ctx.message.content
+            vid_link = URL.split(">play ")[1]
+            with YoutubeDL(YDL_OPTIONS) as ydl:
+                voice = await ctx.author.voice.channel.connect()
+                # ERROR: Unable to extract uploader id; please report this issue on https://yt-dl.org/bug . Make sure
+                # you are using the latest version; see  https://yt-dl.org/update  on how to update. Be sure to call
+                # youtube-dl with the --verbose flag and include its complete output.
                 info = ydl.extract_info(vid_link, download=False)
-                URL = info['formats'][0]['url']
-            voice = await ctx.author.voice.channel.connect()
-            voice.play(discord.FFmpegPCMAudio(URL))
+                audio_url = info['formats'][0]['url']
+                voice.play(discord.FFmpegPCMAudio(audio_url))
 
-    @client.command(pass_context=True)
-    #Additional command access to use this command
+            return {'source': audio_url, 'title': info['title']}
+
+    @bot.command(pass_context=True)
+    # Additional command access to use this command
     async def leave(ctx):
         if ctx.voice_client:
             em = discord.Embed()
@@ -180,7 +160,23 @@ def run_discord_bot():
             em.add_field(name="JOI Music", value="```An Error occurred, I am not in a vc```")
             await ctx.reply(embed=em)
 
-    client.run(config["TOKEN"])
+    @bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.reply('Please pass in all requirements :rolling_eyes:.')
+        elif isinstance(error, commands.CommandInvokeError):
+            await ctx.reply("Error occurred")
+        elif isinstance(error, discord.errors.Forbidden):
+            await ctx.reply("You dont have all the requirements")
+        elif isinstance(error, commands.MemberNotFound):
+            await ctx.reply("Member not found")
+        elif isinstance(error, commands.MissingPermissions):
+            if ctx.command.name in ['kick', 'ban', 'warn']:
+                await ctx.reply("You don't have the required permissions to run this command.")
+            else:
+                await ctx.reply("You dont have all the requirements :angry:")
+
+    bot.run(config["TOKEN"])
 
 
 """After paying for the ChatGPT subscription, you will have the ability to use it in your bot using the code below"""
